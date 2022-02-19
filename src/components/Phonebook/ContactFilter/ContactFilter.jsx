6b55403417a4e2819d68fde325cc5c18c4d28e49
Danyil Dykyi <91718159+bezdarshadow@users.noreply.github.com>
2022-02-19 18:30:33 +0200

    homework

---
+++ src/components/Phonebook/ContactFilter/ContactFilter.jsx
@@ -0,0 +1,21 @@
+import { useRef } from 'react';
+import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
+
+import styles from './contact-filter.module.css'
+const ContactFilter = ({ value, onChange }) => {
+    const filterInputId = useRef(nanoid())
+  return (
+    <>
+      <label className={styles.label} htmlFor={filterInputId.current}> Find contacts by name</label>
+      <input className={styles.input} type="text" name="filter" value={value} onChange={onChange} id={filterInputId.current} />
+    </>
+  );
+};
+
+export default ContactFilter;
+
+ContactFilter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
---
+++ src/components/Phonebook/Phonebook.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect, useRef } from 'react';
+import { nanoid } from 'nanoid';
+
+import ContactFilter from './ContactFilter';
+import ContactList from './ContactList';
+import ContactForm from './ContactForm';
+
+import styles from './phonebook.module.css';
+
+const Phonebook = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+
+  const firstRenderRef = useRef(true)
+
+  useEffect(() => {
+    if(firstRenderRef.current){
+      const contacts = localStorage.getItem('contacts')
+      const parsedContacts = JSON.parse(contacts)
+      if(parsedContacts){
+        setContacts(parsedContacts)
+      }
+      firstRenderRef.current = false;
+      return;
+    }
+
+    localStorage.setItem('contacts', JSON.stringify(contacts))
+
+  }, [contacts])
+ 
+  const addContact = newContact => {
+    const newCont = {
+      ...newContact,
+      id: nanoid(),
+    };
+    if (contacts.find(contact => contact.name === newCont.name)) {
+      alert(`${newCont.name} is already in contacts`);
+      return;
+    }
+
+    setContacts(state => [...state, newCont])
+
+  };
+  const deleteContact = contactId => {
+    setContacts(state => state.filter(contact => contact.id !== contactId));
+  };
+  const changeFilter = event => {
+    const { value } = event.target;
+    setFilter(value)
+  };
+  const filteredContacts = () => {
+    const normalizeFilter = filter.toLowerCase();
+    const contactsList = contacts.filter(contact => contact.name.toLowerCase().includes(normalizeFilter))
+    return contactsList;
+  };
+
+  return (
+    <div className={styles.section}>
+      <h2 className={styles.title}>Phonebook</h2>
+      <ContactForm onChange={addContact} />
+      <h2 className={styles.title}>Contacts</h2>
+      <ContactFilter value={filter} onChange={changeFilter} />
+      <ContactList contacts={filteredContacts()} onDelete={deleteContact} />
+    </div>
+  );
+};
+
+export default Phonebook;
